7ab5933f962dd7509d9f2a9468d9848fd799474b
adomurtad <adomurtado1@gmail.com>
2024-10-10 04:33:01 +0700

    sassung

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle menu
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
